099e2b7f4bd307cb22876831275886dcb3201aa8
Jeff Wright <jeff.washcloth@gmail.com>
2020-10-22 13:30:47 -0400

    Update random_chord_gen.py

--- random_chord_gen.py
+++ random_chord_gen.py
@@ -16,24 +16,22 @@ def chords(ch_num):
     ch_root = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', ]
     #ch_quality = ['', 'm', 'dim', '+']
     ch_quality = ['', 'm']
-    ch_prog = ''
+    ch_prog = '| '
     #generate string of 'num' chords
     for x in range(ch_num):
         rand_root = random.sample(ch_root,1)
-        #rand_root = random.choice(ch_root)
         rand_quality = random.choice(ch_quality)
-        #import pdb; pdb.set_trace()
         ch_prog += str(rand_root[0]+rand_quality+' | ')
-    print('Sharp (#) Version:')
-    print('\t'+ch_prog)
-    #convert string to flats
-    ch_prog  = ch_prog.replace('C#','Db')
-    ch_prog  = ch_prog.replace('D#','Eb')
-    ch_prog  = ch_prog.replace('F#','Gb')
-    ch_prog  = ch_prog.replace('G#','Ab')
-    ch_prog  = ch_prog.replace('A#','Bb')
-    print('Flat (b) Version:')
-    print('\t'+ch_prog)
+    if user_conv == 's':
+        print('\t'+ch_prog)
+    #convert string to flats if desired
+    if user_conv == 'f':
+        ch_prog  = ch_prog.replace('C#','Db')
+        ch_prog  = ch_prog.replace('D#','Eb')
+        ch_prog  = ch_prog.replace('F#','Gb')
+        ch_prog  = ch_prog.replace('G#','Ab')
+        ch_prog  = ch_prog.replace('A#','Bb')
+        print('\t'+ch_prog)
 
 #start
 print('Generate Random Chord Progressions')
@@ -42,6 +40,7 @@ print('Generate Random Chord Progressions')
 try:
     user_num = int(input('How many chords? (rec. 3-10) '))
     user_x = int(input('How many random progressions? (50 max) '))
+    user_conv = str(input('Generate flats or sharps? (f/s) '))
     if user_num > 16: user_num = 16
     if user_num < 1: user_num = 3
     if user_x > 50: user_x = 50
@@ -53,7 +52,7 @@ except:
     user_x = 10
 #main function
 for x in range(1,user_x+1):
-    print('{}/{}: Random Chord Progression of {} chords'.format(x,user_x,user_num))
+    #print('{}/{}: Random Chord Progression of {} chords'.format(x,user_x,user_num))
     chords(user_num)
 '''
 How to save output as text?
